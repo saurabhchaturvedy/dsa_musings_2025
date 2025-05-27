400d70ce5f8ae90bbea13e16f8451cb836216d0e
Chaturvedi, Saurabh <saurabhchaturvedy@gmail.com>
2025-05-28 00:02:50 +0530

    badge access

---
+++ src/main/java/Atlassian/Karat/DomainVisit1.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DomainVisit1 {
+
+
+    public List<String> subdomainVisits(String[] cpdomains) {
+
+        Map<String,Integer> map = new HashMap<>();
+
+        for(String domainEntry : cpdomains)
+        {
+
+            String[]domainInfo = domainEntry.split(" ");
+
+            int visitCount = Integer.parseInt(domainInfo[0]);
+            String domain = domainInfo[1];
+            int length = domain.length();
+
+            if(map.containsKey(domain))
+            {
+
+                map.put(domain, map.get(domain)+visitCount);
+            }
+            else
+            {
+
+                map.put(domain,visitCount);
+            }
+
+            for(int i=0; i<length; i++)
+            {
+
+
+                if(domain.charAt(i)=='.')
+                {
+
+                    String temp = domain.substring(i+1,length);
+
+                    if(map.containsKey(temp))
+                    {
+
+                        map.put(temp, map.get(temp)+visitCount);
+                    }
+                    else
+                    {
+
+                        map.put(temp,visitCount);
+                    }
+                }
+            }
+
+        }
+
+        List<String> result = new ArrayList<>();
+
+        for(Map.Entry<String,Integer> m : map.entrySet())
+        {
+
+
+            int count = m.getValue();
+            String domainValue = m.getKey();
+            result.add(count+" "+domainValue);
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
