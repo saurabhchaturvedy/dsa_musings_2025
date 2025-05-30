d48d3d2b17655d8d87accd364a4fb9920072acc3
Chaturvedi, Saurabh <saurabhchaturvedy@gmail.com>
2025-05-30 22:03:38 +0530

    badge access

---
+++ src/main/java/Atlassian/Karat/KaratFriday/Intervals/InsertInterval.java
@@ -0,0 +1,75 @@
+package KaratFriday.Intervals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InsertInterval {
+
+
+
+
+    public static int[][] insert(int[][] intervals, int[] newInterval) {
+
+
+        List<int[]> result = new ArrayList<>();
+
+        int i=0;
+        int n = intervals.length;
+
+        while(i<n && intervals[i][1] < newInterval[0])
+        {
+
+            result.add(intervals[i]);
+            i++;
+        }
+
+        while(i<n && intervals[i][0] <= newInterval[1])
+        {
+
+            newInterval[0] = Math.min(intervals[i][0],newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1],newInterval[1]);
+            i++;
+        }
+
+        result.add(newInterval);
+
+        while(i<n)
+        {
+
+            result.add(intervals[i]);
+            i++;
+        }
+
+        return result.toArray(new int[result.size()][]);
+
+    }
+
+
+    public static void printIntervals(int[][] intervals) {
+        for (int[] interval : intervals) {
+            System.out.print("[" + interval[0] + "," + interval[1] + "] ");
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+
+
+        int[][] intervals = {
+                {1, 3},
+                {6, 9}
+        };
+        int[] newInterval = {2, 5};
+
+        System.out.println("Original Intervals:");
+        printIntervals(intervals);
+        System.out.println("New Interval: [" + newInterval[0] + "," + newInterval[1] + "]");
+
+        // Call the insert function
+        int[][] merged = insert(intervals, newInterval);
+
+        System.out.println("Merged Intervals:");
+        printIntervals(merged);
+    }
+}
