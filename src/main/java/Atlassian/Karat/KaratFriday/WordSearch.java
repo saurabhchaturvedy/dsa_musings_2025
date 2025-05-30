55c3b2bbe50eb84de1b2ef7acc694ec85fa1cec8
Chaturvedi, Saurabh <saurabhchaturvedy@gmail.com>
2025-05-30 16:05:53 +0530

    badge access

---
+++ src/main/java/Atlassian/Karat/KaratFriday/WordSearch.java
@@ -0,0 +1,82 @@
+package KaratFriday;
+
+public class WordSearch {
+
+
+
+    static int[][]dir = {{1,0},{-1,0},{0,1},{0,-1}};
+
+
+    public static boolean exist(char[][] board, String word) {
+
+
+        int m = board.length;
+        int n = board[0].length;
+
+        for(int i=0; i<m; i++)
+        {
+
+            for(int j=0; j<n; j++)
+            {
+
+                if(board[i][j]==word.charAt(0) && find(board,i,j,0,m,n,word))
+                {
+
+                    return true;
+                }
+            }
+        }
+
+        return false;
+
+    }
+
+
+    public static boolean find(char[][]board,int i,int j, int idx, int m,int n , String word)
+    {
+
+
+        if(idx==word.length()) return true;
+
+        if(i<0 || j<0 || i>=m || j>=n || board[i][j]=='$') return false;
+
+        if(board[i][j]!=word.charAt(idx)) return false;
+
+        char temp = board[i][j];
+
+        board[i][j] = '$';
+
+        for(int[]d : dir)
+        {
+
+            int i_ = i + d[0];
+            int j_ = j + d[1];
+
+            if(find(board,i_,j_,idx+1,m,n,word))
+            {
+
+                return true;
+            }
+        }
+
+
+        board[i][j] = temp;
+        return false;
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+
+        char[][] words = {{'A', 'B', 'C', 'E' }, {'S', 'F', 'C', 'S' }, {'A', 'D', 'E', 'E' }};
+
+        String str = "ABCCED";
+
+
+        boolean exists = exist(words, str);
+
+        System.out.println(" word exists = " + exists);
+    }
+}
