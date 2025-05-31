1f29b3b8f5ba76ea2c5cc971b1f4e7fcf1b3eacc
Chaturvedi, Saurabh <saurabhchaturvedy@gmail.com>
2025-05-31 09:44:29 +0530

    badge access

---
+++ src/main/java/Atlassian/Karat/PreInterview/BadgeAccess1.java
@@ -0,0 +1,91 @@
+package PreInterview;
+
+import java.util.*;
+
+public class BadgeAccess1 {
+
+
+    public static Map<String, Set<String>> badgeAccess1(List<String[]> accessLog) {
+
+
+        Map<String, Boolean> accessMap = new HashMap<>();
+        Set<String> enterWithoutAnExit = new HashSet<>();
+        Set<String> exitWithoutAnEnter = new HashSet<>();
+
+
+        for (String[] record : accessLog) {
+
+            String name = record[0];
+            String action = record[1];
+
+
+            if (action.equals("enter")) {
+
+
+                if (accessMap.getOrDefault(name, false)) {
+
+                    enterWithoutAnExit.add(name);
+                }
+
+                accessMap.put(name, true);
+            } else if (action.equals("exit")) {
+
+
+                if (!accessMap.getOrDefault(name, false)) {
+
+                    exitWithoutAnEnter.add(name);
+                } else {
+
+                    accessMap.put(name, false);
+                }
+
+
+            }
+        }
+
+
+        for (Map.Entry<String, Boolean> entry : accessMap.entrySet()) {
+
+
+            if (entry.getValue()) {
+
+                enterWithoutAnExit.add(entry.getKey());
+            }
+        }
+
+
+        Map<String, Set<String>> result = new HashMap<>();
+        result.put("enterWithoutAnExit", enterWithoutAnExit);
+        result.put("exitWithoutAnEnter", exitWithoutAnEnter);
+
+
+        return result;
+
+    }
+
+
+    public static void main(String[] args) {
+
+
+
+
+        List<String[]> records = Arrays.asList(
+                new String[]{"Martha", "exit"},
+                new String[]{"Paul", "enter"},
+                new String[]{"Martha", "enter"},
+                new String[]{"Martha", "exit"},
+                new String[]{"Jennifer", "enter"},
+                new String[]{"Paul", "enter"},
+                new String[]{"Curtis", "enter"},
+                new String[]{"Paul", "exit"},
+                new String[]{"Martha", "enter"},
+                new String[]{"Martha", "exit"},
+                new String[]{"Jennifer", "exit"}
+        );
+
+        Map<String, Set<String>> result = badgeAccess1(records);
+
+        System.out.println("Entered without exiting: " + result.get("enterWithoutAnExit"));
+        System.out.println("Exited without entering: " + result.get("exitWithoutAnEnter"));
+    }
+}
