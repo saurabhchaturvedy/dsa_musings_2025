c2f985b8cd7c04a9d6e720aba6042e0f6ec5e5a8
Chaturvedi, Saurabh <saurabhchaturvedy@gmail.com>
2025-06-11 19:50:52 +0530

    badge access

--- src/main/java/Atlassian/Karat/PostKarat11June/DataStructures/FileCollection/FileCollectionSystem.java
+++ src/main/java/Atlassian/Karat/PostKarat11June/DataStructures/FileCollection/FileCollectionSystem.java
@@ -4,94 +4,60 @@ import java.util.*;
 
 public class FileCollectionSystem {
 
-
     Map<String, File> files;
     Map<String, Collection> collections;
-    TreeSet<Collection> sortedCollections;
     int totalSize = 0;
 
-
     FileCollectionSystem() {
-
         this.files = new HashMap<>();
         this.collections = new HashMap<>();
         this.totalSize = 0;
-        this.sortedCollections = new TreeSet<>((a, b) -> {
-
-
-            int sizeCompare = b.getTotalSize() - a.getTotalSize();
-            if (sizeCompare != 0) {
-                return sizeCompare;
-            }
-
-            return a.getName().compareTo(b.getName());
-        });
     }
 
-
     public void addFile(String name, Integer size, List<String> collections) {
-
-
         File file = new File(name, size);
         this.files.put(name, file);
         totalSize += file.getSize();
 
-        if(collections!=null) {
-
+        if (collections != null) {
             for (String col : collections) {
-
                 Collection collectionToAddTo = this.collections.computeIfAbsent(col, Collection::new);
                 collectionToAddTo.addFile(file);
-                resetSortedCollections(collectionToAddTo);
             }
         }
     }
 
-
     public void addChildToParent(String childName, String parentName) {
-
         Collection child = collections.computeIfAbsent(childName, Collection::new);
         Collection parent = collections.computeIfAbsent(parentName, Collection::new);
-
         parent.addChildCollection(child);
-        resetSortedCollections(parent);
-
     }
-
-
-    public void resetSortedCollections(Collection collection) {
-
-
-        sortedCollections.remove(collection);
-        collection.getTotalSize();
-        sortedCollections.add(collection);
-
-    }
-
 
     public List<Collection> topKCollections(int k) {
+        PriorityQueue<Collection> minHeap = new PriorityQueue<>((a, b) -> {
+            int sizeCompare = a.getTotalSize() - b.getTotalSize();
+            if (sizeCompare != 0) return sizeCompare;
+            return b.getName().compareTo(a.getName());
+        });
 
-        List<Collection> topkList = new ArrayList<>();
-
-        for (Collection collection : sortedCollections) {
-
-            if (topkList.size() == k) break;
-
-                topkList.add(collection);
-
+        for (Collection collection : collections.values()) {
+            minHeap.offer(collection);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
         }
 
-        return topkList;
+        List<Collection> result = new ArrayList<>();
+        while (!minHeap.isEmpty()) {
+            result.add(minHeap.poll());
+        }
+
+        Collections.reverse(result);
+        return result;
     }
 
     public static void main(String[] args) {
-
-
-
-
         FileCollectionSystem fileCollectionSystem = new FileCollectionSystem();
-
-
 
         fileCollectionSystem.addFile("file1.txt", 100, null);
         fileCollectionSystem.addFile("file2.txt", 200, Arrays.asList("c1"));
@@ -101,7 +67,6 @@ public class FileCollectionSystem {
         fileCollectionSystem.addFile("file6.txt", 400, Arrays.asList("c4"));
         fileCollectionSystem.addFile("file7.txt", 250, Arrays.asList("c5"));
 
-
         fileCollectionSystem.addChildToParent("c2", "c1");
         fileCollectionSystem.addChildToParent("c4", "c5");
         fileCollectionSystem.addChildToParent("c5", "c2");
@@ -109,13 +74,7 @@ public class FileCollectionSystem {
         List<Collection> collectionList = fileCollectionSystem.topKCollections(3);
 
         for (Collection collection : collectionList) {
-
-            System.out.println(collection.name + " " + collection.getTotalSize());
+            System.out.println(collection.getName() + " " + collection.getTotalSize());
         }
-
-
-
     }
-
-
 }
